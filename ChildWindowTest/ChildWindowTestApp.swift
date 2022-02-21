423e45d20792d000878f6bd1d94a2ee7c228a82b
Steven Kirk <grokys@gmail.com>
2022-02-21 17:42:28 +0100

    Initial Commit

---
+++ ChildWindowTest/ChildWindowTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChildWindowTestApp.swift
+//  ChildWindowTest
+//
+//  Created by Steven Kirk on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ChildWindowTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
