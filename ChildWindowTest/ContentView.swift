76d1fd99f9db6505367e7ed06e882dfd3d53ead5
Steven Kirk <grokys@gmail.com>
2022-05-30 23:30:46 +0200

    Show child window.

--- ChildWindowTest/ContentView.swift
+++ ChildWindowTest/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Hello World", action:  openChild)
+            .padding(50)
+    }
+    
+    func openChild()  {
+        let win = NSWindow();
+        win.windowController = ChildWindowController()
+        let parent = NSApp.mainWindow
+        parent?.addChildWindow(win, ordered: NSWindow.OrderingMode.above)
+        win.makeKeyAndOrderFront(self)
+        win.contentView = NSHostingView(rootView: ChildView(win: win))
+    }
+}
+
+class ChildWindowController :NSWindowController, NSWindowDelegate {
+    func windowDidChangeScreen(_ notification: Notification) {
+        
+    }
+}
+
+struct ChildView: View {
+    var _win: NSWindow;
+    
+    init(win: NSWindow) {
+        _win = win
+    }
+    
+    var body: some View {
+        Button("hello", action: debugMe)
+            .padding(50)
+    }
+    
+    func debugMe() {
     }
 }
 
